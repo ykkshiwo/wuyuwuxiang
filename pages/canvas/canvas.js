76b4862b6205c15e59929a4ae681bd1a10565b74
yangkangkai <452306233@qq.com>
2018-06-09 17:14:26 +0800

    20180609

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -8,16 +8,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    yuliu_w: 0,
+    yuliu_h: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
     wx.getSystemInfo({
       success: function(res) {
         console.log(res.screenHeight, res.screenWidth)
+        var yuliu_w = res.screenWidth * 0.05
+        var yuliu_h = res.screenHeight * 0.08
+        that.setData({
+          yuliu_w: yuliu_w,
+          yuliu_h: yuliu_h
+        })
       },
     })
   },
@@ -33,15 +41,24 @@ Page({
       var p = map[key]
       const longs = p['0']
       const lats = p['1']
-      context.moveTo(longs[0], 200-lats[0])
+      context.moveTo(this.longToZB(longs[0], 412), this.latToZB(lats[0], 732))
       for (var i = 1; i < longs.length; i++) {
-        context.lineTo(longs[i], 200-lats[i])
+        context.lineTo(this.longToZB(longs[i], 412), this.latToZB(lats[i], 732))
       }
     }
     context.stroke()
     context.draw()
   },
 
+  longToZB: function(long, sw){
+    var r = 0.9 * (long - 73.38) * sw / 61.62 + 73.38 - (73.38 - this.data.yuliu_w)
+    return r
+  },
+
+  latToZB: function(lat, sh){
+    var r = sh * 0.7 * ( 53.6 - lat ) * 0.018 + this.data.yuliu_h
+    return r
+  },
   /**
    * 生命周期函数--监听页面显示
    */
